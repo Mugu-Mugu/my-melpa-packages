483a2cf165b701b0f61d88404c1b9cddd5e98174
Mugu <amardavid3241@gmail.com>
2018-01-09 06:03:26 +0100

    Update melpa archives

---
+++ no-littering-20180108.305/no-littering-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "no-littering" "20180108.305" "help keeping ~/.emacs.d clean" '((cl-lib "0.5")) :commit "e585ca177a55808a17a53e8a9020198a18175543" :url "https://github.com/emacscollective/no-littering")
